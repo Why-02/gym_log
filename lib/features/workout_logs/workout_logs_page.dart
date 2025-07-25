d3ccf613f103b3b5004ee9769df6b4542e374083
jw-02-pfda <johnywakim02@gmail.com>
2025-07-25 19:13:52 +0300

    added a placeholder in workout_logs page

---
+++ lib/features/workout_logs/workout_logs_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WorkoutLogsPage extends StatelessWidget {
+  const WorkoutLogsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
